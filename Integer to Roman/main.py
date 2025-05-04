cca818b64f5ba354bb2888e1da5aecafc3401a6f
Salman Ahmed <say2salmanahmed@gmail.com>
2025-05-04 20:21:41 +0500

    Integer to Roman

---
+++ Integer to Roman/main.py
@@ -0,0 +1,21 @@
+lst =  [
+    (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+    (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+    (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+]
+
+n = int(input())
+if n == 0 or n < 0:
+    print("Invalid Integer")
+    exit()
+
+roman = ""
+
+while(n):
+    for val in lst:
+        if val[0] <= n:
+            roman += val[1]
+            n -= val[0]
+            break
+
+print(roman)
